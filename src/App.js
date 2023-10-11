ac515a7180bfe3d0e375d3a7d636990e0e232b72
hariharan1212 <hariharanmech1212@gmail.com>
2023-10-11 18:31:03 +0530

    code modified

--- src/App.js
+++ src/App.js
@@ -14,13 +14,12 @@ export default function App() {
   return (
     <>
     <div className="appContainer">
-       <div className="appPage">
+      <div className="appPage">
         <div className="appPage_nav">
-          <h3>film-hook   
-             <span><button><GrAdd/></button></span>
-             <span><button><GrNotification/></button></span>
-          </h3>
-        </div> <hr />
+          <h3>film-hook   </h3>
+             <span className="appPage_nav_btn1"><button><GrAdd/></button></span>
+             <span className="appPage_nav_btn2"><button><GrNotification/></button></span>
+        </div> 
        
         <div className="appPage_route">
               <Routes>
@@ -31,15 +30,15 @@ export default function App() {
                     <Route path="/profile" element={<Profile/>} />
               </Routes>
         </div>
-       </div> <hr/>
+       </div> 
 
        <div className="appIcons">
-          <span> <Link to={"/"}><img src="./images/home.png" alt=''/> </Link> </span>
-          <span> <Link to={"/chat"}><img src="./images/chat.png" alt=''/> </Link> </span>
-          <span> <Link to={"/searchclint"}><img src="./images/personSearch.png" alt=''/></Link> </span>
-          <span> <Link to={"/navigation"}><img src="./images/navigation.png" alt=''/> </Link> </span>
-          <span> <Link to={"/profile"}><img src="./images/userprofile.png" alt=''/> </Link> </span>
-       </div>
+          <Link to={"/"} ><img src="./images/home.png" alt='' /></Link> 
+          <Link to={"/chat"}><img src="./images/chat.png" alt=''/></Link> 
+          <Link to={"/searchclint"}><img src="./images/personSearch.png" alt=''/></Link> 
+          <Link to={"/navigation"}><img src="./images/navigation.png" alt=''/></Link> 
+          <Link to={"/profile"}><img src="./images/userprofile.png" alt=''/></Link> 
+      </div>
     </div>
     </>
   );
